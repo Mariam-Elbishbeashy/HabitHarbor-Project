b5c4508d1a2ff413882ce5995cf1bc2d125a1c18
SondosGafar <sondos2202126@miuegypt.edu.eg>
2024-04-30 06:50:18 +0300

    Added a health info section and modified info section in user profile

--- scripts/user.js
+++ scripts/user.js
@@ -5,35 +5,53 @@ function toggleOption(optionName) {
   var generalContent = document.getElementById("generalContent");
   var infoContent = document.getElementById("infoContent");
   var passwordContent = document.getElementById("passwordContent");
+  var healthInfoContent = document.getElementById("healthInfoContent");
 
   var generalLink = document.getElementById("generalLink");
   var infoLink = document.getElementById("infoLink");
   var passwordLink = document.getElementById("passwordLink");
+  var healthInfoLink = document.getElementById("healthInfoLink"); 
 
   if (optionName === "general") {
       generalContent.style.display = "block";
       infoContent.style.display = "none";
       passwordContent.style.display = "none";
+      healthInfoContent.style.display = "none"; 
 
       generalLink.classList.add('active');
       infoLink.classList.remove('active');
       passwordLink.classList.remove('active');
+      healthInfoLink.classList.remove('active'); 
   } else if (optionName === "info") {
       generalContent.style.display = "none";
       infoContent.style.display = "block";
       passwordContent.style.display = "none";
+      healthInfoContent.style.display = "none"; 
 
       generalLink.classList.remove('active');
       infoLink.classList.add('active');
       passwordLink.classList.remove('active');
+      healthInfoLink.classList.remove('active'); 
   } else if (optionName === "password") {
       generalContent.style.display = "none";
       infoContent.style.display = "none";
       passwordContent.style.display = "block";
+      healthInfoContent.style.display = "none"; 
 
       generalLink.classList.remove('active');
       infoLink.classList.remove('active');
       passwordLink.classList.add('active');
+      healthInfoLink.classList.remove('active'); 
+  } else if (optionName === "healthInfo") { 
+      generalContent.style.display = "none";
+      infoContent.style.display = "none";
+      passwordContent.style.display = "none";
+      healthInfoContent.style.display = "block";
+
+      generalLink.classList.remove('active');
+      infoLink.classList.remove('active');
+      passwordLink.classList.remove('active');
+      healthInfoLink.classList.add('active');
   }
 }
 
@@ -79,3 +97,53 @@ window.onload = function() {
     fileInput.click();
   }
  
+  function toggleOtherTextArea() {
+    var otherCheckbox = document.getElementById("goal5");
+    var otherTextArea = document.getElementById("otherGoalTextArea");
+    if (otherCheckbox.checked) {
+        otherTextArea.style.display = "block";
+    } else {
+        otherTextArea.style.display = "none";
+    }
+}
+
+function saveHealthInfoChanges() {
+  var checkboxes = document.querySelectorAll('#healthInfoContent input[type="checkbox"]');
+  var otherTextArea = document.getElementById("otherGoal");
+  var saveChangesButton = document.querySelector('#healthInfoContent button');
+
+  checkboxes.forEach(function(checkbox) {
+    checkbox.disabled = true;
+  });
+
+  otherTextArea.disabled = true;
+
+  saveChangesButton.style.display = "none";
+  saveChangesButton.style.border = "none";
+
+  var instructionLine = document.querySelector('.custom-info');
+  instructionLine.style.display = "none";
+
+  checkboxes.forEach(function(checkbox) {
+    checkbox.classList.add('disabled-checkbox');
+  });
+
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  var closeModalBtn = document.getElementById("closeModalBtn");
+  closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
