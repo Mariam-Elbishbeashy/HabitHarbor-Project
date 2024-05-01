0caee3c3361c76c33d74d376b78ede52e929f3d2
LauraLucasZ <lauralucas060@gmail.com>
2024-05-01 16:55:56 +0300

    edited checkbox & trash-btn and added validation of home page activities

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //event listener that shows the progress bar after clicking on the start button
 document.addEventListener("click", function(event) {
-    if (event.target.classList.contains("start-btn")) {
+    if (event.target.id === "start-btn") {
         var button = event.target;
         button.style.display = "none";
         var activityContainer = button.closest(".wactivity");
@@ -9,8 +9,12 @@ document.addEventListener("click", function(event) {
         if (activityContainer) {
             var loader = activityContainer.querySelector(".loader");
             var progress = activityContainer.querySelector(".progress");
+            var completionBtn = activityContainer.querySelector("#completion-btn");
+
             loader.style.visibility = "visible";
             progress.style.visibility = "visible";
+            completionBtn.style.visibility = "visible";
+
             var progressValue = 0;
             activityContainer.style.setProperty("--progress-value", progressValue + "%");
             progress.innerHTML = progressValue + "%";
@@ -19,8 +23,12 @@ document.addEventListener("click", function(event) {
         else if(activityContainer2){
             var loader = activityContainer2.querySelector(".loader");
             var progress = activityContainer2.querySelector(".progress");
+            var completionBtn = activityContainer2.querySelector("#completion-btn");
+
             loader.style.visibility = "visible";
             progress.style.visibility = "visible";
+            completionBtn.style.visibility = "visible";
+
             var progressValue = 0;
             activityContainer2.style.setProperty("--progress-value", progressValue + "%");
             progress.innerHTML = progressValue + "%";
@@ -28,3 +36,22 @@ document.addEventListener("click", function(event) {
         }
     }
 });
+function dailyValidate() {
+    var inputTitle = document.querySelector("#input-title");
+    var inputContent = document.querySelector("#input-content");
+
+    // Check if either input is empty
+    if (inputTitle.value === "" && inputContent.value === "") {
+        alert("The challenge title and its content cannot be empty.");
+    } else if (inputTitle.value === "") {
+        alert("The challenge title cannot be empty.");
+    } else if (inputContent.value === "") {
+        alert("The challenge content cannot be empty.");
+    }
+}
+function weeklyMonthlyValidate(){
+    var input = document.querySelector("#custom-activity");
+    if(input.value === ""){
+        alert("The challenge content can not be ampty")
+    }
+}
