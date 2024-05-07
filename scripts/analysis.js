e06d493729b1e36f6d0cb433bef8f28fad14f632
AhmedH2004 <duduhesham2004.ah.ah@gmail.com>
2024-05-07 20:39:22 +0300

    Before Phase 1 Sub

---
+++ scripts/analysis.js
@@ -0,0 +1,38 @@
+function toggle(component, clickedBtn) {
+    const buttons = document.querySelectorAll('.switch-container');
+    const components = document.querySelectorAll('.content > span');
+
+    buttons.forEach(btn => {
+        if (btn === clickedBtn) {
+            btn.classList.add('active'); // Add active class to clicked button
+        } else {
+            btn.classList.remove('active'); // Remove active class from other buttons
+        }
+    });
+
+    components.forEach(comp => {
+        if (comp.classList.contains(component)) {
+            if (component === "history") {
+                comp.style.display = 'grid';
+                comp.style.gridTemplateColumns = '1fr 1fr';
+                comp.style.gridTemplateRows = '0.4fr 1.6fr 0.4fr 1.6fr 0.4fr 1.6fr';
+                comp.style.gap = '10px 10px';
+                comp.style.gridAutoFlow = 'row';
+                comp.style.gridTemplateAreas = `
+                    "title title"
+                    "daily-challenges daily-challenges"
+                    "ttitle ttitle"
+                    "weekly-challenges weekly-challenges"
+                    "Title Title"
+                    "monthly-challenges monthly-challenges"
+                `;
+                comp.style.overflowY = 'auto'; // Enable scrolling only on this part
+            }
+            else{
+                comp.style.display = 'block';
+            }
+        } else {
+            comp.style.display = 'none';
+        }
+    });
+}
